c2192f76db840e22b3bacfa7320a0acd7c4c35eb
Eugene Tan <>
2024-02-22 20:23:35 +0800

    init commit

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from pymongo import MongoClient
 import config
 from langchain_community.llms import Ollama
+from langchain.prompts import PromptTemplate
 
 # Initialize MongoDB client
 uri = config.mongo_uri
@@ -18,7 +19,7 @@ collection = client[db_name][coll_name]
 
 # Initialize text embedding model (encoder)
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-index_name = "vector_index"
+index_name = "vector_ollama"
 vector_field_name = "plot_embedding_hf"
 text_field_name = "title"
 
@@ -31,11 +32,13 @@ vectorStore = MongoDBAtlasVectorSearch(
     text_key=text_field_name,
 )
 
+# vectorStore = MongoDBAtlasVectorSearch(collection, embeddings)
+
 # Callbacks support token-wise streaming
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
 # Run the LLM (quantized local version by The Bloke's)
-llm = Ollama(model="llama2")
+llm = Ollama(model="llama2", callback_manager=callback_manager)
 
 
 # Streamlit App
@@ -45,20 +48,40 @@ def main():
     # User input
     query = st.text_input("Enter your query:")
 
-    # Retrieve context data from MongoDB Atlas Vector Search
-    retriever = vectorStore.as_retriever()  # Modify this line
+    retriever = vectorStore.as_retriever()
+    # Modify this line
+
+    prompt_template = """
+    <s>[INST] <<SYS>>
+    Use the following context to answer the question at the end as the only source of truth. The following contain movie titles along with the associated movie description. 
+    Just share the movie title and description as it is provided in the context.
+    The movie titles and their corresponding plots are as follow:
+    <</SYS>>
+    {context}
+
+    Question: {question}[/INST]
+    """
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
 
     # Query LLM with user input and context data
     if st.button("Query LLM"):
         with st.spinner("Querying LLM..."):
             qa = RetrievalQA.from_chain_type(
-                llm, chain_type="stuff", retriever=retriever
+                llm,
+                chain_type="stuff",
+                retriever=retriever,
+                return_source_documents=True,
+                chain_type_kwargs={"prompt": PROMPT},
             )
-
-            response = qa({"query": query})
+            # retriever_output = qa.run(query)
+            retriever_output = qa(query)
+            # response = qa({"query": query})
 
             st.text("Llama2 Response:")
-            st.text(response["result"])
+            st.text(retriever_output)
 
 
 if __name__ == "__main__":
